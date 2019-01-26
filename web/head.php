db451c7d486cb4bc233e942355de9a85c64957b1
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-01-25 23:27:04 -0500

    fixing errors with uri

--- web/head.php
+++ web/head.php
@@ -9,13 +9,13 @@
     ';
 
     // $_SERVER['HTTP_HOST'];
-    // $_SERVER['REQUEST_URL'];
-    if ($_SERVER['request_URI'] == "https://radiant-beach-62520.herokuapp.com/homepage.php" && "https://radiant-beach-62520.herokuapp.com/assignment.php")  {
+    // $_SERVER['REQUEST_URI'];
+    if ($_SERVER['REQUEST_URI'] == "/homepage.php" && "/assignment.php")  {
         echo '<link href="homepage-main.css" type="text/css" rel="stylesheet">
               <link href="homepage-medium.css" type="text/css" rel="stylesheet">
               <link href="homepage-large.css" type="text/css" rel="stylesheet">
             ';
-    } else if ($_SERVER['REQUEST_URI'] == "https://radiant-beach-62520.herokuapp.com/browseShop_wk3prove.php" && "https://radiant-beach-62520.herokuapp.com/checkout_wk3prove.php") {
+    } else if ($_SERVER['REQUEST_URI'] == "/browseShop_wk3prove.php" && "/checkout_wk3prove.php") {
         echo '<link href="checkout_wk3prove.css" type="text/css" rel="stylesheet">';
     }
 
