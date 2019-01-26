0c13f2f2b90481345d953d7f966e38a0b1dcc028
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-01-25 22:55:12 -0500

    adding images and files for wk3 prove

---
+++ web/browseShop_wk3prove.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <title>Browse Items</title>
+        <?php
+                include ('head.php');
+        ?>
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <main>
+            <div class="item">
+                <img src="/imgs/avocado-toast.jpeg" class="foodImage crop" alt="photo of avocado toast">
+            </div>
+            <div class="item">
+                <img src="/imgs/burger.jpeg" class="foodImage crop" alt="photo of a burger"> 
+            </div>
+            <div class="item">
+                <img src="/imgs/burrito.jpeg" class="foodImage crop" alt="photo of a burrito">
+            </div>
+            <div class="item">
+                <img src="/imgs/chicken-salad.jpeg" class="foodImage crop" alt="photo of a salad with chicken">
+            </div>
+            <div class="item">
+                <img src="/imgs/chocolate-cake.jpeg" class="foodImage crop" alt="photo of chocolate cake">
+            </div>
+            <div class="item">
+                <img src="/imgs/doughnut.jpeg" class="foodImage crop" alt="photo of a doughnut">
+            </div>
+            <div class="item">
+                <img src="/imgs/dumpling-soup.jpeg" class="foodImage crop" alt="photo of dumpling soup">
+            </div>
+            <div class="item">
+                <img src="/imgs/fries-n-sandwich.jpeg" class="foodImage crop" alt="photo of fries and a sandwich">
+            </div>
+            <div class="item">
+                <img src="/imgs/fruit.png" class="foodImage crop" alt="Various fruits on a plate">
+            </div>
+            <div class="item">
+                <img src="/imgs/kimchi.jpeg" class="foodImage crop" alt="photo of kimchi">
+            </div>
+            <div class="item">
+                <img src="/imgs/lamb.jpeg" class="foodImage crop" alt="photo of a rack of lamb">
+            </div>
+            <div class="item">
+                <img src="/imgs/noodles.jpeg" class="foodImage crop" alt="photo of noodles">
+            </div>
+            <div class="item">
+                <img src="/imgs/pancakes.jpeg" class="foodImage crop" alt="photo of pancakes">
+            </div>
+            <div class="item">
+                <img src="/imgs/parfait.jpeg" class="foodImage crop" alt="photo of a parfait">
+            </div>
+            <div class="item">
+                <img src="/imgs/penne-pasta.jpeg" class="foodImage crop" alt="photo of penne pasta">
+            </div>
+            <div class="item">
+                <img src="/imgs/pho.jpeg" class="foodImage crop" alt="photo of pho">
+            </div>
+            <div class="item">
+                <img src="/imgs/pizza.jpeg" class="foodImage crop" alt="photo of pizza">
+            </div>
+            <div class="item">
+                <img src="/imgs/rice-n-egg.jpeg" class="foodImage crop" alt="photo of an egg over rice">
+            </div>
+            <div class="item">
+                <img src="/imgs/salad.jpeg" class="foodImage crop" alt="photo of salad">
+            </div>
+            <div class="item">
+                <img src="/imgs/soup.jpeg" class="foodImage crop" alt="photo of soup">
+            </div>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
