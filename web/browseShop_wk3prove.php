0cd8014eefa707cfb34b7d7d2b151584de9799e6
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-01-27 01:11:09 -0500

    css and class

--- web/browseShop_wk3prove.php
+++ web/browseShop_wk3prove.php
@@ -16,63 +16,103 @@
             <div id="item-holder">
                 <div class="item">
                     <img src="/imgs/avocado-toast.jpeg" class="foodImage crop" alt="photo of avocado toast">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
-                    <img src="/imgs/burger.jpeg" class="foodImage crop" alt="photo of a burger"> 
+                    <img src="/imgs/burger.jpeg" class="foodImage crop" alt="photo of a burger">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button> 
                 </div>
                 <div class="item">
                     <img src="/imgs/burrito.jpeg" class="foodImage crop" alt="photo of a burrito">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/chicken-salad.jpeg" class="foodImage crop" alt="photo of a salad with chicken">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/chocolate-cake.jpeg" class="foodImage crop" alt="photo of chocolate cake">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/doughnut.jpeg" class="foodImage crop" alt="photo of a doughnut">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/dumpling-soup.jpeg" class="foodImage crop" alt="photo of dumpling soup">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/fries-n-sandwich.jpeg" class="foodImage crop" alt="photo of fries and a sandwich">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/fruit.png" class="foodImage crop" alt="Various fruits on a plate">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/kimchi.jpeg" class="foodImage" alt="photo of kimchi">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/lamb.jpeg" class="foodImage" alt="photo of a rack of lamb">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/noodles.jpeg" class="foodImage" alt="photo of noodles">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/pancakes.jpeg" class="foodImage" alt="photo of pancakes">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/parfait.jpeg" class="foodImage" alt="photo of a parfait">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/penne-pasta.jpeg" class="foodImage" alt="photo of penne pasta">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/pho.jpeg" class="foodImage" alt="photo of pho">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/pizza.jpeg" class="foodImage" alt="photo of pizza">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/rice-n-egg.jpeg" class="foodImage" alt="photo of an egg over rice">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/salad.jpg" class="foodImage" alt="photo of salad">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/soup.jpeg" class="foodImage" alt="photo of soup">
+                    <span class="foodName hidden"></span>
+                    <button class="hidden">Add to Cart</button>
                 </div>
             </div>
         </main>
