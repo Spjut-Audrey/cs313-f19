d26e4e8c95b6af3000c0153c70d403aa596a003b
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-01-27 01:44:15 -0500

    moving everything right in div

--- web/browseShop_wk3prove.php
+++ web/browseShop_wk3prove.php
@@ -17,82 +17,82 @@
                 <div class="item">
                     <img src="/imgs/avocado-toast.jpeg" class="foodImage crop" alt="photo of avocado toast">
                     <span class="foodName ">Avocado Toast</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/burger.jpeg" class="foodImage crop" alt="photo of a burger">
                     <span class="foodName ">Burger</span>
-                    <button class="">Add to Cart</button> 
+                    <button class="addButton">Add to Cart</button> 
                 </div>
                 <div class="item">
                     <img src="/imgs/burrito.jpeg" class="foodImage crop" alt="photo of a burrito">
                     <span class="foodName ">Burrito</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/chicken-salad.jpeg" class="foodImage crop" alt="photo of a salad with chicken">
                     <span class="foodName ">Salad with Chicken</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/chocolate-cake.jpeg" class="foodImage crop" alt="photo of chocolate cake">
                     <span class="foodName ">Chocolate Cake</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/doughnut.jpeg" class="foodImage crop" alt="photo of a doughnut">
                     <span class="foodName ">Doughnut</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/dumpling-soup.jpeg" class="foodImage crop" alt="photo of dumpling soup">
                     <span class="foodName ">Dumpling Soup</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/fries-n-sandwich.jpeg" class="foodImage crop" alt="photo of fries and a sandwich">
                     <span class="foodName ">Fries and Sandwich</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/fruit.png" class="foodImage crop" alt="Various fruits on a plate">
                     <span class="foodName ">Fruit</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/kimchi.jpeg" class="foodImage" alt="photo of kimchi">
                     <span class="foodName ">Kimchi</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/lamb.jpeg" class="foodImage" alt="photo of a rack of lamb">
                     <span class="foodName ">Lamb</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/noodles.jpeg" class="foodImage" alt="photo of noodles">
                     <span class="foodName ">Noodles</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/pancakes.jpeg" class="foodImage" alt="photo of pancakes">
                     <span class="foodName ">Pancakes</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/parfait.jpeg" class="foodImage" alt="photo of a parfait">
                     <span class="foodName ">Parfait</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/penne-pasta.jpeg" class="foodImage" alt="photo of penne pasta">
-                    <span class="foodName ">Penne Pasta</span>
-                    <button class="">Add to Cart</button>
+                    <span class="foodName">Penne Pasta</span>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/pho.jpeg" class="foodImage" alt="photo of pho">
                     <span class="foodName ">Pho</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/pizza.jpeg" class="foodImage" alt="photo of pizza">
@@ -102,17 +102,17 @@
                 <div class="item">
                     <img src="/imgs/rice-n-egg.jpeg" class="foodImage" alt="photo of an egg over rice">
                     <span class="foodName ">Rice with Egg</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/salad.jpg" class="foodImage" alt="photo of salad">
                     <span class="foodName ">Salad</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/soup.jpeg" class="foodImage" alt="photo of soup">
                     <span class="foodName "></span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton">Add to Cart</button>
                 </div>
             </div>
         </main>
