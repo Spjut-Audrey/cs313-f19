00903974044fb059a78a7c1be9742e19a00ba731
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-01-27 23:03:51 -0500

    fixing head css

--- web/browseShop_wk3prove.php
+++ web/browseShop_wk3prove.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+  session_start();
+?><!DOCTYPE html>
 <html lang="en-us">
     <head>
         <title>Browse Items</title>
@@ -17,92 +19,92 @@
                 <div class="item">
                     <img src="/imgs/avocado-toast.jpeg" class="foodImage crop" alt="photo of avocado toast">
                     <span class="foodName ">Avocado Toast</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/burger.jpeg" class="foodImage crop" alt="photo of a burger">
                     <span class="foodName ">Burger</span>
-                    <button class="addButton">Add to Cart</button> 
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/burrito.jpeg" class="foodImage crop" alt="photo of a burrito">
                     <span class="foodName ">Burrito</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/chicken-salad.jpeg" class="foodImage crop" alt="photo of a salad with chicken">
                     <span class="foodName ">Salad with Chicken</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/chocolate-cake.jpeg" class="foodImage crop" alt="photo of chocolate cake">
                     <span class="foodName ">Chocolate Cake</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/doughnut.jpeg" class="foodImage crop" alt="photo of a doughnut">
                     <span class="foodName ">Doughnut</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/dumpling-soup.jpeg" class="foodImage crop" alt="photo of dumpling soup">
                     <span class="foodName ">Dumpling Soup</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/fries-n-sandwich.jpeg" class="foodImage crop" alt="photo of fries and a sandwich">
                     <span class="foodName ">Fries and Sandwich</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/fruit.png" class="foodImage crop" alt="Various fruits on a plate">
                     <span class="foodName ">Fruit</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/kimchi.jpeg" class="foodImage" alt="photo of kimchi">
                     <span class="foodName ">Kimchi</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/lamb.jpeg" class="foodImage" alt="photo of a rack of lamb">
                     <span class="foodName ">Lamb</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/noodles.jpeg" class="foodImage" alt="photo of noodles">
                     <span class="foodName ">Noodles</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/pancakes.jpeg" class="foodImage" alt="photo of pancakes">
                     <span class="foodName ">Pancakes</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/parfait.jpeg" class="foodImage" alt="photo of a parfait">
                     <span class="foodName ">Parfait</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/penne-pasta.jpeg" class="foodImage" alt="photo of penne pasta">
                     <span class="foodName">Penne Pasta</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/pho.jpeg" class="foodImage" alt="photo of pho">
                     <span class="foodName ">Pho</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/pizza.jpeg" class="foodImage" alt="photo of pizza">
                     <span class="foodName ">Pizza</span>
-                    <button class="">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/rice-n-egg.jpeg" class="foodImage" alt="photo of an egg over rice">
                     <span class="foodName ">Rice with Egg</span>
-                    <button class="addButton">Add to Cart</button>
+                    <button class="addButton" action="cartFunctions.php">Add to Cart</button>
                 </div>
                 <div class="item">
                     <img src="/imgs/salad.jpg" class="foodImage" alt="photo of salad">
--- web/head.php
+++ web/head.php
@@ -3,14 +3,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, inital-scale=1">
         <meta name="author" content="Audrey Spjut">
-        
+
         <!-- js here -->
         <script src="/homepage.js"></script>
     ';
 
     // $_SERVER['HTTP_HOST'];
     // $_SERVER['REQUEST_URI'];
-    if ($_SERVER['REQUEST_URI'] == "/homepage.php" && "/assignment.php")  {
+    if ($_SERVER['REQUEST_URI'] == "/homepage.php" || "/assignment.php")  {
         echo '<link href="homepage-main.css" type="text/css" rel="stylesheet">
               <link href="homepage-medium.css" type="text/css" rel="stylesheet">
               <link href="homepage-large.css" type="text/css" rel="stylesheet">
