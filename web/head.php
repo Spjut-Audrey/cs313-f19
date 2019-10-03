a72c6c935268d60c0e82e36b680af8a506aae066
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-03 16:36:01 -0400

    adding css link

--- web/head.php
+++ web/head.php
@@ -2,5 +2,7 @@
     echo '<meta charset=utf-8>
           <meta name="author" content="Audrey Spjut">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          
+          <link rel="stylesheet" type="text/css" href="homepage.css">
           '
 ?>
