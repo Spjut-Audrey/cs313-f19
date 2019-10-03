9d33ba2a18028218051fd8f11d984931b5a65992
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-03 18:55:52 -0400

    css and blizzcon info

--- web/homepage.php
+++ web/homepage.php
@@ -21,6 +21,14 @@
 
                     echo "There are $countdown days until Blizzcon!";
                 ?>
+            <p>Blizzcon is a really fun way to learn more about the 
+                gaming company Blizzard, their games and competitions.
+
+                I love playing Blizzards' various games. Overwatch, World
+                of Warcraft (Battle of Azeroth and Classic),
+                Heroes of the Storm, Diablo, Hearthstone, and just a
+                little Starcraft.
+            </p>
         </div>
     </body>
 </html>
