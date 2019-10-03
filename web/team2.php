5aa160a216297542a9dec78ce447b6400bd63a80
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-03 14:42:47 -0400

    adding jquery toggle hide show stuff

--- web/team2.php
+++ web/team2.php
@@ -8,10 +8,7 @@
 
     <!-- SCRIPT LINKS HERE -->
     <script src="team2.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">        $("#hideDiv").click(function() {
-        $("#hiddenDiv").toggle(500);
-        });
-    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
     <main>
