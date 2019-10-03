a029a6e69e6cd74785b74b5af91563f972d40e8c
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-03 18:12:11 -0400

    css

--- web/head.php
+++ web/head.php
@@ -3,6 +3,7 @@
           <meta name="author" content="Audrey Spjut">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           
+          <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
           <link rel="stylesheet" type="text/css" href="homepage.css">
           '
 ?>
--- web/sidebar.php
+++ web/sidebar.php
@@ -1,6 +1,6 @@
 <?php            
     echo '
-            <img src="audrey-spjut.JPG" id="profilePic" alt="profile pic of Audrey Spjut">
-            <a href="https://github.com/Spjut-Audrey/cs313-f19">Github CS 313 Repo</a>       
+            <img src="audrey-spjut.JPG" id="profilePic" alt="profile pic of Audrey Spjut"><br>
+            <a href="https://github.com/Spjut-Audrey/cs313-f19">Github CS 313 Repo</a><br>       
     '
 ?>
