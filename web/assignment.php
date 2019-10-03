07ce55e5b77afbe58dcf5e65cc54c424cbc0e348
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-03 18:59:45 -0400

    adding assignment content

--- web/assignment.php
+++ web/assignment.php
@@ -14,7 +14,20 @@
             <a href="homepage.php">Back to Homepage</a>
         </div>
         <div class="content">
-
+            <a href="hello.html">Week 1: Hello World</a>
+            <a href="team2.php">Week 2: Team Activity</a>
+            <a href="">Week 3: Team Activity</a>
+            <a href="">Week 4</a>
+            <a href="">Week 5</a>
+            <a href="">Week 6</a>
+            <a href="">Week 7</a>
+            <a href="">Week 8</a>
+            <a href="">Week 9</a>
+            <a href="">Week 10</a>
+            <a href="">Week 11</a>
+            <a href="">Week 12</a>
+            <a href="">Week 13</a>
+            <a href="">Week 14</a>
         </div>
     </body>
 </html>
