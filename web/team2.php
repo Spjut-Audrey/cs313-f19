c581417a966c228718016137b561953fa1238e14
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-03 13:39:21 -0400

    js div changing stuff

--- web/team2.php
+++ web/team2.php
@@ -8,9 +8,15 @@
 </head>
 <body>
     <main>
-        <div >I am some text in div 1</div>
+        <div id="div1">I am some text in div 1</div>
+        <textarea id="textArea" rows="3" cols="30">
+            Enter new color name
+        </textarea>
+        <button onclick="changeColor()">Change Div Color</button>
+
         <div>I am div two</div>
         <div>Woah another div? Div 3</div>
+
         <button id="button" onclick="buttonAlert()">Click Me!</button>
     </main>
 </body>
