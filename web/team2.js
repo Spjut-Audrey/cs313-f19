6d02551859bc16c6c102fe1d6d15977028b315b5
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-03 13:38:21 -0400

    adding color changing div js

--- web/team2.js
+++ web/team2.js
@@ -1,3 +1,10 @@
 function buttonAlert() {
     alert("Clicked!");
 }
+
+function changeColor() {
+    var changedColor = document.getElementById("textArea").value;
+
+    document.document.getElementById("div1").style.backgroundColor = changedColor;
+
+}
