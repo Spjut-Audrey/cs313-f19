8d235ca2e62fb8aeca203770e08c65e924f94a69
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-03 12:30:06 -0400

    week two team assignment initial commit

---
+++ web/team2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>    
+    <title>Week 2: Team</title>
+
+    <!-- CSS LINKS -->
+</head>
+<body>
+    <div></div>
+    <div></div>
+    <div></div>
+
+    <button>Click Me!</button>
+</body>
+<!-- JS LINKS HERE -->
+</html>
