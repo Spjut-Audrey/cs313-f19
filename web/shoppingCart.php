5362d810ebb16c5a81e681a86046ba92adada5c6
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-17 12:36:47 -0400

    css and header

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -12,21 +12,24 @@
         <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
     </head>
     <body>
+        <header>
+            <h1>Shopping Cart</h1>
+        </header>
         <div class="grid-container">
             <div class="gI-1">
                 <img src="https://www.nintendo.com/content/dam/noa/en_US/hardware/switch/nintendo-switch-new-package-gray/gallery/package_gray.jpg" alt="Nintendo Switch"><br>
                 <span>Nintendo Switch<br>$299.99</span><br>
-                <a href="">Add to Cart</a>
+                <a href="">Add to Cart</a><br>
             </div>
             <div class="gI-2">
                 <img src="https://target.scene7.com/is/image/Target/GUEST_1d8adc19-6218-4934-8bcc-4bcf22bf786d?fmt=webp&wid=1400&qlt=80" alt="Nintendo Switch Joy-cons"><br>
                 <span>Nintendo Switch Joy-cons<br>$66.99</span><br>
-                <a href="">Add to Cart</a>
+                <a href="">Add to Cart</a><br>
             </div>
             <div class="gI-3">
                 <img src="https://target.scene7.com/is/image/Target/GUEST_2a10c7ce-3fd2-4490-8b98-c8e7d73542a2?fmt=webp&wid=1400&qlt=80" alt="Nintendo Switch Pro Controller"><br>
                 <span>Nintendo Switch Pro Controller<br>$62.99</span><br>
-                <a href="">Add to Cart</a>
+                <a href="">Add to Cart</a><br>
             </div>
         </div>
     </body>
