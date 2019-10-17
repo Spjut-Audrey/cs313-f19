1d7dfb207116b72c9d729e273268395c5dce1c3f
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-17 12:27:01 -0400

    inline grid

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -14,7 +14,6 @@
             <div class="gI-1">
                 <img src="https://www.nintendo.com/content/dam/noa/en_US/hardware/switch/nintendo-switch-new-package-gray/gallery/package_gray.jpg" alt="Nintendo Switch"><br>
                 <span>Nintendo Switch<br>$299.99</span>
-                <
             </div>
             <div class="gI-2">
                 <img src="https://target.scene7.com/is/image/Target/GUEST_1d8adc19-6218-4934-8bcc-4bcf22bf786d?fmt=webp&wid=1400&qlt=80" alt="Nintendo Switch Joy-cons"><br>
