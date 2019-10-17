f095a5f8f9abe705bb776ec206285250b758f346
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-17 12:19:42 -0400

    adding shopping cart assignment base

---
+++ web/shoppingCart.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>Shopping Cart | CS313</title>
+
+        <!-- css -->
+        <link rel="stylesheet" type="text/css" href="shoppingCart.css">
+    </head>
+    <body>
+        <div class="grid-container">
+            <div class="gI-1">
+                <img src="https://www.nintendo.com/content/dam/noa/en_US/hardware/switch/nintendo-switch-new-package-gray/gallery/package_gray.jpg" alt="Nintendo Switch">
+                <span>Nintendo Switch<br>$299.99</span>
+                <
+            </div>
+            <div class="gI-2">
+                <img src="https://target.scene7.com/is/image/Target/GUEST_1d8adc19-6218-4934-8bcc-4bcf22bf786d?fmt=webp&wid=1400&qlt=80" alt="Nintendo Switch Joy-cons">
+                <span>Nintendo Switch Joy-cons<br>$66.99</span>
+            </div>
+            <div class="gI-3">
+                <img src="https://target.scene7.com/is/image/Target/GUEST_2a10c7ce-3fd2-4490-8b98-c8e7d73542a2?fmt=webp&wid=1400&qlt=80" alt="Nintendo Switch Pro Controller">
+                <span>Nintendo Switch Pro Controller<br>$62.99</span>
+            </div>
+        </div>
+    </body>
+</html>
