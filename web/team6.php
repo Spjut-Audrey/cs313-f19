62c28710f6cd6dd98527ca6605fade5f574635e8
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-10-22 13:22:10 -0400

    team 6

---
+++ web/team6.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <h1>Scripture Resources</h1>
+</head>
+<body>
+​
+    <?php
+       try
+       {
+         $dbUrl = getenv('DATABASE_URL');
+       
+         $dbOpts = parse_url($dbUrl);
+       
+         $dbHost = $dbOpts["host"];
+         $dbPort = $dbOpts["port"];
+         $dbUser = $dbOpts["user"];
+         $dbPassword = $dbOpts["pass"];
+         $dbName = ltrim($dbOpts["path"],'/');
+       
+         $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+       
+         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       }
+       catch (PDOException $ex)
+       {
+         echo 'Error!: ' . $ex->getMessage();
+         die();
+       }
+​
+    //    foreach ($db->query('SELECT book, chapter, verse, content FROM Scriptures') as $row)
+    //    {
+    //      echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . ' - ' . '</strong>' . '"' . $row['content'] . '"' . '<br>'; 
+    //    }
+    ?> 
+
+        <form method="POST" action="">
+            Book:<input type="text"><br>
+            Chapter:<input type="text"><br>
+            Verse:<input type="text"><br>
+            Content:<textarea rows="50" cols="50">
+            <?php 
+                foreach ($db->query('SELECT topic FROM topic') as $row) {
+                    echo '<input type="checkbox">' . $row['topic'];
+                }
+            ?>
+
+            <input type="submit" value="Submit">
+        </form>
+​
+</body>
+</html>
