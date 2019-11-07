fd8b0c5e426de029992a377c597a3be952bdc3a4
ALLERIA\Audrey <ellen.audrey@gmail.com>
2019-11-07 12:28:11 -0500

    removing br's?

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -26,21 +26,21 @@
             <form method="post" action="cartView.php">
                 <div class="gI-1">
                     <!-- make sure to set items to session for passing data to cart page -->
-                    <img src="https://www.nintendo.com/content/dam/noa/en_US/hardware/switch/nintendo-switch-new-package-gray/gallery/package_gray.jpg" alt="Nintendo Switch"><br>
-                    <span>Nintendo Switch<br>$299.99</span><br><br>
-                    <a href="">Add to Cart</a><br>
+                    <img src="https://www.nintendo.com/content/dam/noa/en_US/hardware/switch/nintendo-switch-new-package-gray/gallery/package_gray.jpg" alt="Nintendo Switch">
+                    <p>Nintendo Switch<br>$299.99</p>
+                    <p><a href="">Add to Cart</a><p>
                 </div>
                 <div class="gI-2">
                     <!-- make sure to set items to session for passing data to cart page -->
-                    <img src="https://target.scene7.com/is/image/Target/GUEST_1d8adc19-6218-4934-8bcc-4bcf22bf786d?fmt=webp&wid=1400&qlt=80" alt="Nintendo Switch Joy-cons"><br>
-                    <span>Nintendo Switch Joy-cons<br>$66.99</span><br><br>
-                    <a href="">Add to Cart</a><br>
+                    <img src="https://target.scene7.com/is/image/Target/GUEST_1d8adc19-6218-4934-8bcc-4bcf22bf786d?fmt=webp&wid=1400&qlt=80" alt="Nintendo Switch Joy-cons">
+                    <p>Nintendo Switch Joy-cons<br>$66.99</p>
+                    <p><a href="">Add to Cart</a><p>
                 </div>
                 <div class="gI-3">
                     <!-- make sure to set items to session for passing data to cart page -->
-                    <img src="https://target.scene7.com/is/image/Target/GUEST_2a10c7ce-3fd2-4490-8b98-c8e7d73542a2?fmt=webp&wid=1400&qlt=80" alt="Nintendo Switch Pro Controller"><br>
-                    <span>Nintendo Switch Pro Controller<br>$62.99</span><br><br>
-                    <a href="">Add to Cart</a><br>
+                    <img src="https://target.scene7.com/is/image/Target/GUEST_2a10c7ce-3fd2-4490-8b98-c8e7d73542a2?fmt=webp&wid=1400&qlt=80" alt="Nintendo Switch Pro Controller">
+                    <p>Nintendo Switch Pro Controller<br>$62.99</p>
+                    <p><a href="">Add to Cart</a><p>
                 </div>
                 <input type="image" src="https://cdn.pixabay.com/photo/2013/07/12/14/53/cart-148964_960_720.png" alt="shopping cart graphic" id="shoppingCart" class="img">
             </form>
